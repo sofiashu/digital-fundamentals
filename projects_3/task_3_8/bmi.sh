c4c2b23f3496884e49c3e547160e0410ecaf6af6
Shurmeleva Sofia <sofia.shurmeleva@yandex.ru>
2026-03-18 23:26:18 +0300

    Update bmi.sh

--- projects_3/task_3_8/bmi.sh
+++ projects_3/task_3_8/bmi.sh
@@ -2,4 +2,4 @@
 
 read -p "Введите свою массу в кг: " mass
 read -p "Введите сой рост в м: " height
-echo $"scale=2; $mass / ( $height * $height )" | bc
+echo "scale=2; $mass / ( $height * $height )" | bc
